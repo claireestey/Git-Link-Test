bd2f42fc7706206081f7edda513dd745cc2cf9d5
clairee <esteyc@mcmaster.ca>
2024-02-01 10:04:48 -0500

    heart

--- commit test.R
+++ commit test.R
@@ -1,4 +1,3 @@
-gittestx = c(1.5, 1, 2, 3, 4, 5, 4.5)
-gittesty = c(5, 3, 2, 1, 2, 3, 5)
-plot(gittestx, gittesty)
-hello World
+gittestx = c(3, 2, 1.25, 1.5, 2, 3, 4, 4.5, 4.75, 4, 3)
+gittesty = c(6, 8, 6.5, 5, 3, 1, 3, 5, 6.5, 8, 6)
+plot(gittestx, gittesty, type = "b", xlim = c(0, 6), ylim = c(0, 8.5))
